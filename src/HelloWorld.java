d420f5b6bb11df0a344e6e6fd458c6fb788a400e
Jürgen F <juergen@falb.at>
2022-09-08 20:37:13 +0200

    Fixed typo and modified text

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("A second line");
-        System.out.println("A thrid line");
+        System.out.println("A second line!");
+        System.out.println("A third line!");
     }
 }
