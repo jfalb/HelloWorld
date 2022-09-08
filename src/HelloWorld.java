dc4fb01729a984624c16845bd2214a8054787ede
Juergen Falb <j.falb@nousdigital.net>
2022-09-08 20:36:08 +0200

    Fixed typo

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("A second line");
-        System.out.println("A thrid line");
+        System.out.println("A third line");
     }
 }
